d58c4804c041b2ff502f335807b7d79f9bd35ac6
Pedro Sanders <sanderspedro@gmail.com>
2021-12-08 18:50:08 -0500

    Cleanup

--- .scripts/start_voice.sh
+++ .scripts/start_voice.sh
@@ -4,7 +4,7 @@
 # INTENTS_ENGINE_AGENT="353dc74a-d0c3-406c-bf6c-7e1585af4ae5" \
 # LANGUAGE_CODE="en_US" \
 EVENTS_ENABLED="true" \
-WELCOME_INTENT_TRIGGER="helloslang" \
+WELCOME_INTENT_TRIGGER="hi" \
 TTS_ENGINE="google" \
 ASR_ENGINE="google" \
 INTENTS_ENGINE="dialogflow.es" \
