35e36fd0e4788c5b8a4225c560fe1ce9a94aa2f0
Pedro Sanders <sanderspedro@gmail.com>
2021-12-08 18:49:18 -0500

    Updated dependencies

--- .scripts/start_voice.sh
+++ .scripts/start_voice.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # ACTIVATION_INTENT="bot.activate" \
+# INTENTS_ENGINE_LOCATION="us-central1" \
+# INTENTS_ENGINE_AGENT="353dc74a-d0c3-406c-bf6c-7e1585af4ae5" \
+# LANGUAGE_CODE="en_US" \
 EVENTS_ENABLED="true" \
-WELCOME_INTENT_TRIGGER="hi" \
+WELCOME_INTENT_TRIGGER="helloslang" \
 TTS_ENGINE="google" \
-TTS_VOICE="en-US-Wavenet-F" \
-INTENTS_ENGINE="dialogflow.cx" \
-INTENTS_ENGINE_LOCATION="us-central1" \
-INTENTS_ENGINE_AGENT="353dc74a-d0c3-406c-bf6c-7e1585af4ae5" \
 ASR_ENGINE="google" \
-GOOGLE_CONFIG=$(pwd)/.config/google.json node dist/voice.js
+INTENTS_ENGINE="dialogflow.es" \
+TTS_VOICE="en-US-Wavenet-F" \
+GOOGLE_CONFIG=$(pwd)/google.json ./bin/run
