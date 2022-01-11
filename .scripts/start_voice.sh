15f7b2a5e8c307d2cdbd2547866df34107aa5992
Pedro Sanders <sanderspedro@gmail.com>
2022-01-11 09:39:47 -0500

    Added transfer variables

--- .scripts/start_voice.sh
+++ .scripts/start_voice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 EVENTS_ENABLED="true" \
-WELCOME_INTENT_TRIGGER="hello" \
+WELCOME_INTENT_TRIGGER="WELCOME" \
 TTS_ENGINE="google" \
 ASR_ENGINE="google" \
 INTENTS_ENGINE="dialogflow.es" \
@@ -8,8 +8,7 @@ INTENTS_ENGINE_PLATFORM="TELEPHONY" \
 TTS_VOICE="en-US-Wavenet-F" \
 OTL_EXPORTER_ZIPKIN_URL="http://localhost:9412/api/v2/spans" \
 OTL_EXPORTER_JAEGER_URL="http://localhost:14268/api/traces" \
-TRANSFER_MESSAGE="No worries, we are transfering you" \
-TRANSFER_MEDIA="beep" \
+TRANSFER_MESSAGE="<speak data-intent-id=\"1.0\" data-ui-intent=\"Transfer Message\"><par><media xml:id='a'><speak><s data-class=\"transfer\">Please wait while we transfer you.</s></speak></media><media xml:id=\"speak\" begin=\"a.end-1s\"><audio soundLevel=\"-15dB\" src=\"https://www.kozco.com/tech/organfinale.mp3\"/></media></par></speak>" \
 TRANSFER_MESSAGE_BUSY="Looks like everybody is busy. Try again later." \
 TRANSFER_MESSAGE_NOANSWER="No human available at this time. Try again later." \
 GOOGLE_CONFIG_FILE=$(pwd)/google.json ./bin/run
